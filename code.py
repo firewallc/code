9161af4d149fe68b553992c7fa87bfed8cd64975
firewallc <68114776+firewallc@users.noreply.github.com>
2021-04-13 16:40:05 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,12 @@
+import random, string, requests
+f=open("Valid Nitro.txt", "w", encoding='utf-8')
+
+while True:
+    code = ('').join(random.choices(string.ascii_letters + string.digits, k=16))
+    r = requests.get(f"https://discordapp.com/api/v6/entitlements/gift-codes/{code}?with_application=false&with_subscription_plan=true")
+    if r.status_code == 200:
+        print(f"Valid Nitro Code > discord.gift/{code}")
+        f.write(f"discord.gift/{code}\n")
+    else:
+        print(f"Invalid Nitro Code > discord.gift/{code}")
+        
